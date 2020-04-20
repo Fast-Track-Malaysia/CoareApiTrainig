5683d8a17a79b5c3dc26a362a030324060de72f7
pitboon <epb@myfastrack.net>
2020-04-20 21:14:56 +0800

    Add project files.

---
+++ CoareApiTrainig/Controllers/LoginController.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using CoareApiTrainig.Model;
+using Dapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+
+namespace CoareApiTrainig.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        private IConfiguration _config;
+        public LoginController(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] UserModel data)
+        {
+
+            UserModel login = new UserModel();
+            login.UserID = data.UserID;
+            login.Password = data.Password;
+            IActionResult response = Unauthorized();
+
+            var user = AuthenticateUser(login);
+            if (user != null)
+            {
+                var tokenStr = GenerateJasonWebToken(user);
+                response = Ok(new { token = tokenStr });
+            }
+            return response;
+
+
+        }
+
+        private string GenerateJasonWebToken(UserModel user)
+        {
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Jwt:Key"]));
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+            var claims = new[]
+            {
+                new Claim(JwtRegisteredClaimNames.Sub,user.UserID),
+                new Claim(JwtRegisteredClaimNames.GivenName,user.UserName),
+                new Claim(JwtRegisteredClaimNames.Jti,Guid.NewGuid().ToString())
+            };
+            var token = new JwtSecurityToken(
+                issuer: _config["Jwt:Issuer"],
+                audience: _config["Jwt:Issuer"],
+                claims,
+                expires: DateTime.Now.AddDays(10),
+                signingCredentials: credentials);
+
+            var encodetoken = new JwtSecurityTokenHandler().WriteToken(token);
+            return encodetoken;
+        }
+
+        private UserModel AuthenticateUser(UserModel login)
+        {
+            string connstr = _config["ConnectionStrings:Default"].ToString();
+
+            using (IDbConnection conn = new SqlConnection(connstr))
+            {
+                string query = "SELECT UserID, UserName, Email FROM AppUser Where UserID = @UserID AND Password = @Password ";
+                UserModel usr = SqlMapper.Query<UserModel>(conn, query, new { UserID = login.UserID, Password= login.Password }).FirstOrDefault();
+
+                return usr;
+            }
+
+        }
+    }
+}
---
+++ CoareApiTrainig/Controllers/TestingController.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using CoareApiTrainig.Model;
+using Dapper;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace CoareApiTrainig.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestingController : ControllerBase
+    {
+        private IConfiguration configuration;
+
+        public TestingController(IConfiguration config)
+        {
+            configuration = config;
+        }
+        // GET: api/Testing
+        [HttpGet("GetIdentity")]
+        public IEnumerable<string> Get()
+        {
+            var identity = HttpContext.User.Identity as ClaimsIdentity;
+            IList<Claim> claim = identity.Claims.ToList();
+            return new string[] { claim[0].Value, claim[1].Value, claim[2].Value };
+        }
+
+        // GET: api/Testing/5
+        [Authorize]
+        [HttpGet("{id}", Name = "Get")]
+        public IActionResult Get(string id)
+        {
+            string connstr = configuration["ConnectionStrings:Default"];
+            DataTable dt = new DataTable();
+            IList<UserModel> users = new List<UserModel>();
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(connstr))
+                {
+                    string query = "SELECT UserID, UserName, Email FROM AppUser ";
+                    users = SqlMapper.Query<UserModel>(conn, query, null).ToList();
+
+                    //using (SqlDataAdapter dat = new SqlDataAdapter("", conn))
+                    //{
+                    //    dat.SelectCommand.CommandText = "SELECT * FROM AppUser ";
+                    //    dat.Fill(dt);
+                    //    foreach(DataRow row in dt.Rows)
+                    //    {
+                    //        UserModel usr = new UserModel
+                    //        {
+                    //            UserID = row["UserID"].ToString(),
+                    //            UserName = row["UserName"].ToString(),
+                    //            Email = row["Email"].ToString()
+                    //        };
+                    //        users.Add(usr);
+                    //    }
+                    //}
+                }
+            }
+            catch(Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+            return Ok( users);
+        }
+
+        // POST: api/Testing
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT: api/Testing/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
